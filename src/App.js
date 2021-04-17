652d854f9d4aad2b89df8667f02661a37fd5821f
Sean <jb@JBs-MacBook-Pro.local>
2021-04-17 12:06:58 -0500

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-// import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const cohort = 14;
+  let [counter, setCounter] = useState(0);
+
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <>
-      {cohort === 14 ? <h1>Welcome to Cohort 14!</h1> : <h1>Wrong Cohort!</h1>}
+      <h1>Counter</h1>
+      {counter}
+      <button onClick={() => setCounter(counter + 1)}>Increase by 1</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrease by 1</button>
+      <button onClick={() => setCounter(counter * 3.14)}>Multiply by 3.14</button>
+      <button onClick={() => setCounter(counter = 0)}>Reset</button>
     </>
   );
 }
