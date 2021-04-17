d0bbdaed72d2e2e2f16ec27ec6e2a55be278ea6b
Sean <jb@JBs-MacBook-Pro.local>
2021-04-17 13:19:48 -0500

    added input fields

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import React, { useState } from 'react';
 import './App.css';
+import React from 'react'
+import Counter from './Counter'
 
 function App() {
-  let [counter, setCounter] = useState(0);
-
   return (
-    <>
-      <h1>Counter</h1>
-      {counter}
-      <button onClick={() => setCounter(counter + 1)}>Increase by 1</button>
-      <button onClick={() => setCounter(counter - 1)}>Decrease by 1</button>
-      <button onClick={() => setCounter(counter * 3.14)}>Multiply by 3.14</button>
-      <button onClick={() => setCounter(counter = 0)}>Reset</button>
-    </>
-  );
+      <Counter />
+  )
 }
 
 export default App;
